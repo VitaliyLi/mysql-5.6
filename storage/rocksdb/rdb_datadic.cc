410662f798237fe59cbcbd055826d1ae61723b4d
Vasile Paraschiv <vasilep@fb.com>
2016-03-28 14:12:43 -0700

    Improve code style for MyRocks snapshot and transaction classes
    Summary:
    Class renames:
      SnapshotNotifier => Rdb_snapshot_notifier
      ShowSnapshotStatus => Rdb_snapshot_status
      TransactionListWalker => Rdb_trx_list_walker
    
    Also, touch Rdb_transaction, to make member names start with m_.
    
    Move SHIP_ASSERT macro to rdb_utils.h.
    
    Introduce C-style namespaces: "my_core" for MySQL core API exposed to plugins.
    
    Consolidate around "tx" as the abbreviation for "transaction, in lieu of "txn" or "trx".
    
    Test Plan:
    mysqlbuild.sh --clean
    mysqltest.sh
    mysqltest.sh --testset=RocksDB
    mysqlbuild.sh --clean --valgrind
    mysqltest.sh --nobuild --valgrind rocksdb.rocksdb
    
    Reviewers: jkedgar
    
    Reviewed By: jkedgar
    
    Subscribers: webscalesql-eng
    
    Projects: #webscalesql
    
    Differential Revision: https://reviews.facebook.net/D55989

--- storage/rocksdb/rdb_datadic.cc
+++ storage/rocksdb/rdb_datadic.cc
@@ -1756,7 +1756,7 @@ uchar* Table_ddl_manager::get_hash_key(Rdb_tbl_def *rec, size_t *length,
                                        my_bool not_used __attribute__((unused)))
 {
   *length= rec->m_dbname_tablename.length();
-  return str_to_uchar_ptr(&rec->m_dbname_tablename);
+  return rdb_str_to_uchar_ptr(&rec->m_dbname_tablename);
 }
 
 
@@ -2309,7 +2309,7 @@ int Table_ddl_manager::put(Rdb_tbl_def *tbl, bool lock)
     mysql_rwlock_wrlock(&rwlock);
 
   rec= reinterpret_cast<Rdb_tbl_def*>(
-          find(str_to_uchar_ptr(&tbl->m_dbname_tablename),
+          find(rdb_str_to_uchar_ptr(&tbl->m_dbname_tablename),
                tbl->m_dbname_tablename.length(), false));
   if (rec)
   {
@@ -2322,7 +2322,7 @@ int Table_ddl_manager::put(Rdb_tbl_def *tbl, bool lock)
     index_num_to_keydef[tbl->m_key_descr[keyno]->get_gl_index_id()]=
       std::make_pair(
         std::basic_string<uchar>(
-          str_to_uchar_ptr(&tbl->m_dbname_tablename),
+          rdb_str_to_uchar_ptr(&tbl->m_dbname_tablename),
           tbl->m_dbname_tablename.length()
         ),
         keyno
